ad1c542320bde87cd1157dcd9780d57874350560
Patrila Tangwell <111756874+pattang192@users.noreply.github.com>
2025-04-21 19:15:25 -0400

    Implement functions to start and repeat the game

--- lib/main.rb
+++ lib/main.rb
@@ -5,3 +5,21 @@ require "rubocop"
 require_relative "game"
 require_relative "mastermind"
 require_relative "player"
+
+def play_game
+  game = Game.new
+  game.play
+  repeat_game
+end
+
+def repeat_game
+  puts "Would you like to play again? y/n"
+  input = gets.chomp.downcase
+  if input == "y"
+    play_game
+  else
+    puts "Thank you for playing."
+  end
+end
+
+play_game
