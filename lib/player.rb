a763a7f87acd2afde453f259c6f7eccd0aa35e39
Patrila Tangwell <111756874+pattang192@users.noreply.github.com>
2025-04-21 19:25:05 -0400

    Add function that generates a random code for computer player

--- lib/player.rb
+++ lib/player.rb
@@ -7,3 +7,28 @@ class Player
   include Mastermind
   include Game
 end
+
+# Contains functions unique to computer player
+class ComputerPlayer < Player
+  def random_color
+    COLORS[COLORS.keys[rand(6)]]
+  end
+
+  def colors
+    colors = ""
+    COLORS.each do |key, value|
+      colors << " #{value} #{key}"
+    end
+    colors
+  end
+
+  def generate_code
+    code = ""
+    i = 0
+    while i < 4
+      code << " #{random_color}"
+      i += 1
+    end
+    code
+  end
+end
