71f96537eab45480c2b90c7367c8a86e8a8a854b
Patrila Tangwell <111756874+pattang192@users.noreply.github.com>
2025-04-21 19:18:38 -0400

    Creat game pieces

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -2,4 +2,14 @@
 
 # Contains basic components (functions and variables) of the game
 module Mastermind
+  BLUE = "##".blue.on_blue
+  YELLOW = "##".light_yellow.on_light_yellow
+  RED = "##".red.on_red
+  WHITE = "##".light_white.on_light_white
+  GREEN = "##".light_green.on_light_green
+  PURPLE = "##".magenta.on_magenta
+  GRAY = "##".gray.on_gray
+
+  COLORS = { "blue" => BLUE, "yellow" => YELLOW, "red" => RED, "white" => WHITE,
+             "green" => GREEN, "purple" => PURPLE }.freeze
 end
