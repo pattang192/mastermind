ae7736c88516044356583fcf498830369e9b87e9
Patrila Tangwell <111756874+pattang192@users.noreply.github.com>
2025-04-24 09:01:43 -0400

    Implement functions to receive human player's guess

--- lib/player.rb
+++ lib/player.rb
@@ -32,3 +32,28 @@ class ComputerPlayer < Player
     code
   end
 end
+
+# Contains functions unique to human player
+class HumanPlayer < Player
+  def input_guess
+    puts "Enter your guess, separated by spaces. (Eg. red blue red blue)"
+    guess = gets.chomp.downcase.strip.split
+    if code_valid?(guess)
+      convert_to_pegs(guess)
+    else
+      puts "Invalid input".red
+      input_guess
+    end
+  end
+
+  def code_valid?(code)
+    code.size == 4 && code.all? { |color| COLORS.include?(color) }
+  end
+
+  def convert_to_pegs(array)
+    array = array.map do |color|
+      COLORS[color]
+    end
+    array.join(" ")
+  end
+end
