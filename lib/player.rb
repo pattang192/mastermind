ba8dec68ddfc16771d0086a8f3824ba66c770cf1
Patrila Tangwell <111756874+pattang192@users.noreply.github.com>
2025-04-27 06:49:59 -0400

    Add initialize method

--- lib/player.rb
+++ lib/player.rb
@@ -5,23 +5,15 @@ require_relative "game"
 # Contains functions shared by human and computer player
 class Player
   include Mastermind
-  include Game
+
+  def initialize(board, hint_board)
+    @board = board
+    @hint_board = hint_board
+  end
 end
 
 # Contains functions unique to computer player
 class ComputerPlayer < Player
-  def random_color
-    COLORS[COLORS.keys[rand(6)]]
-  end
-
-  def colors
-    colors = ""
-    COLORS.each do |key, value|
-      colors << " #{value} #{key}"
-    end
-    colors
-  end
-
   def generate_code
     code = ""
     i = 0
@@ -32,8 +24,12 @@ class ComputerPlayer < Player
     code.strip
   end
 
-  def check_guess
-    code = @code.split
+  def random_color
+    COLORS[COLORS.keys[rand(6)]]
+  end
+
+  def check_guess(code)
+    code = code.split
     guess = @board.last.split
     correct_positions = find_correct_positions(code, guess)
     remove_correct_positions(correct_positions, code, guess)
@@ -68,28 +64,21 @@ class ComputerPlayer < Player
     wrong_positions
   end
 
-  def print_board
-    @board.each_with_index do |guess, i|
-      puts "\n#{guess} Guess #{i + 1} \t Hint: #{@hint_board[i]}"
-    end
-  end
-
   def give_hint(num_red_pegs, num_white_pegs)
     hint = ""
     hint << (" #{RED}" * num_red_pegs) << (" #{WHITE}" * num_white_pegs)
     hint << (" #{GRAY}" * (4 - (num_red_pegs + num_white_pegs))) if (num_red_pegs + num_white_pegs) < 4
     @hint_board << hint
-    print_board
   end
 end
 
 # Contains functions unique to human player
 class HumanPlayer < Player
-  def input_guess
+  def input_guess(board)
     puts "Enter your guess, separated by spaces. (Eg. red blue red blue)"
     guess = gets.chomp.downcase.strip.split
     if code_valid?(guess)
-      convert_to_pegs(guess)
+      board << convert_to_pegs(guess)
     else
       puts "Invalid input".red
       input_guess
